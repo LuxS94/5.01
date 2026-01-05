77fa0769353bf06794e47cc71ca14dfccf20301f
LuxS94 <simonetti.lucio@libero.it>
2026-01-05 16:56:46 +0100

    n

---
+++ 5.01/src/PrimoEs.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PrimoEs {
+    Scanner scanner = new Scanner(System.in);
+
+
+    //3
+    String[] elements = new String[5];
+    String one = scanner.nextLine();
+
+
+    public static String concat(String[] elements, String one) {
+        String[] nuova = new String[6];
+        nuova[0] = elements[0];
+        nuova[1] = elements[1];
+        nuova[2] = one;
+        nuova[3] = elements[2];
+        nuova[4] = elements[3];
+        nuova[5] = elements[4];
+        return nuova;
+    }
+
+    //1
+    public static int product(int x, int y) {
+        return x * y;
+    }
+
+    //2
+    public static String conc(String a, String b) {
+        return a + b;
+    }
+
+};
+
+
+
